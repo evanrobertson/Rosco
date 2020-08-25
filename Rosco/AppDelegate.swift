6f165a0008e36ca30b8b18f2fb26d39035320b38
evanrobertson <evanjonr@gmail.com>
2020-08-25 10:54:11 +1000

    Change to status bar app, remove dock icon

--- Rosco/AppDelegate.swift
+++ Rosco/AppDelegate.swift
@@ -10,11 +10,16 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var menu: NSMenu!
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     
     var nowPlayingService: NowPlayingService?
     
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
         nowPlayingService = NowPlayingService()
+        statusItem.button?.image = NSImage.init(named: NSImage.Name("status_bar_icon"))
+        statusItem.menu = menu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
